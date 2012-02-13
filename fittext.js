b75c49c33290855b3bb40076438eaebedfdd66c5
Michael Wheeler <michael.wheeler@tapjoy.com>
2012-02-12 22:22:49 -0800

    Passing jshint

--- fittext.js
+++ fittext.js
@@ -1,4 +1,5 @@
-/*global jQuery or ender */
+/*global jQuery,$,ender,window,document*/
+/*jshint maxerr:10,indent:2,laxcomma:true*/
 /*!	
  * FitText.js 1.0
  *
@@ -9,54 +10,57 @@
  * Date: Thu May 05 14:23:00 2011 -0600
  */
 
-(function( $ ){
+(function ($) {
+  "use strict";
+
   var resizeOccurred = true
     , clearStyles
+    , setup_throttle
     ;
 
-  clearStyles = function($$) {
+  clearStyles = function ($$) {
     var args = arguments
       , i, ii
       ;
 
-    for(i=1, ii=args.length; i<ii; i++) {
+    for (i = 1, ii = args.length; i < ii; i++) {
       $$.css(args[i], "");
     }
   };
 
-  function setup_throttle() {
-    $(window).resize(function() {
+  setup_throttle = function () {
+    $(window).resize(function () {
       resizeOccurred = true;
     });
 
-    window.setInterval(function() {
-      if(resizeOccurred) {
+    window.setInterval(function () {
+      if (resizeOccurred) {
         resizeOccurred = false;
         $(document).trigger("fittext-resize");
       }
     }, 300);
 
-    setup_throttle = function() {};
-  }
+    setup_throttle = function () {};
+  };
 
-  $.fn.vAlign = function() {
-    return this.each(function() {
+  $.fn.vAlign = function () {
+    return this.each(function () {
       var $$ = $(this)
         , $p = $$.parent()
         , resizer
         , resetter
         ;
 
-      resetter = function() {
+      resetter = function () {
         clearStyles($$, "position", "top", "margin-top");
         $(document).unbind("fittext-resize", resizer);
         $(document).unbind("fittext-reset", resetter);
       };
-      resizer = function() {
+      resizer = function () {
         $$.css({
-          "position":"relative", 
-          "top":($p.height()/2),
-          "margin-top":(-$$.height()/2)
+          "position" : "relative", 
+          "top" : ($p.height() / 2),
+          "margin-top" : (-$$.height() / 2)
         });
       };
 
@@ -65,30 +69,30 @@
     });
   };
 
-  $.fn.fitText = function( kompressor, options ) {
+  $.fn.fitText = function (kompressor, options) {
     var settings = {
       'minFontSize' : Number.NEGATIVE_INFINITY,
       'maxFontSize' : Number.POSITIVE_INFINITY
     };
 
-    return this.each(function(){
+    return this.each(function () {
       var $this = $(this);              // store the object
       var compressor = kompressor || 1; // set the compressor
-      if ( options ) { 
-         $.extend( settings, options );
+      if (options) { 
+        $.extend(settings, options);
       }
 
       setup_throttle();
 
       // Resizer() resizes items based on the object width divided by the compressor * 10
       var resizer = function () {
-        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor * 10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
       };
 
       // Call once to set.
       resizer();
 
-      var resetter = function() {
+      var resetter = function () {
         clearStyles($this, "font-size");
         $(document).unbind("fittext-resize", resizer);
         $(document).unbind("fittext-reset", resetter);
@@ -99,4 +103,4 @@
     });
   };
 
-})( this.jQuery || this.ender );
+})(this.jQuery || this.ender);
